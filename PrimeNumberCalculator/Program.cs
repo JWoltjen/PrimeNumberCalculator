a8bc76b8d4caa694efc645ce1cd62061a686d6bb
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-07-24 11:26:58 -0500

    refactor PrintIsPrime so that printFactors will only fire if number is not prime.

--- PrimeNumberCalculator/Program.cs
+++ PrimeNumberCalculator/Program.cs
@@ -6,14 +6,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter a number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
-        var factors = GetFactors(number);
-        var primeFactors = GetPrimeFactors(factors);
-
+        int number = Convert.ToInt32(Console.ReadLine());// it will return the input as a string, so to perform math calculations on the input, we must use Convert.ToInt32
         PrintIsPrime(number);
-        Console.WriteLine($"The number {number} has {factors.Count} unique factors.");
-        Console.WriteLine("The prime factors are: " + string.Join(", ", primeFactors));
-        Console.WriteLine("The largest prime factor is: " + (primeFactors.Count > 0 ? primeFactors[primeFactors.Count - 1].ToString() : "None"));
     }
 
     static void PrintIsPrime(int number)
@@ -25,7 +19,17 @@ class Program
         else
         {
             Console.WriteLine($"{number} is not a prime number");
+            PrintFactors(number);
         }
+    }
+
+    static void PrintFactors(int number)
+    {
+        var factors = GetFactors(number);
+        var primeFactors = GetPrimeFactors(factors);
+        Console.WriteLine($"The number {number} has {factors.Count} unique factors.");
+        Console.WriteLine("The prime factors are: " + string.Join(", ", primeFactors));
+        Console.WriteLine("The biggest prime factor is: " + (primeFactors.Count > 0 ? primeFactors[primeFactors.Count - 1]: "None"));
     }
 
     static bool isPrime (int number)
@@ -33,13 +37,12 @@ class Program
         if (number <= 1) return false;
         if (number == 2) return true;
         if (number % 2 == 0) return false;
-
-        var boundary = (int)Math.Floor(Math.Sqrt(number));
         /*
-         * 
-         * The reason this works is due to the properties of factors. For a number n, if it has a factor greater than its square root, then there must be a corresponding factor that is less than its square root. For example, if n is 36, its square root is 6. The factors of 36 are 1, 2, 3, 4, 6, 9, 12, 18, and 36. Notice that for every factor greater than 6, there is a corresponding factor less than 6.
-         */
-        for (int i = 3; i <= boundary; i += 2)//already checked evens can't be prime
+        * The reason this works is due to the properties of factors. For a number n, if it has a factor greater than its square root, then there must be a corresponding factor that is less than its square root. For example, if n is 36, its square root is 6. The factors of 36 are 1, 2, 3, 4, 6, 9, 12, 18, and 36. Notice that for every factor greater than 6, there is a corresponding factor less than 6.
+        */
+        var boundary = (int)Math.Floor(Math.Sqrt(number));
+       
+        for (int i = 3; i <= boundary; i += 2)//set loop at just beyond special base cases. already checked evens can't be prime
         {
             if (number % i == 0)
                 return false;
